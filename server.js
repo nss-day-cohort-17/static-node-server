46d662e9f218f8f500c02bf9b424ae37e67b8a7f
Scott Humphries <git@sscotth.io>
2016-11-16 13:33:43 -0600

    Disable cache

--- server.js
+++ server.js
@@ -6,5 +6,12 @@ const directory = process.argv[2] || '.'
 const port = process.env.PORT || 8080
 
 express()
+.use((req, res, next) => {
+  // http://stackoverflow.com/questions/49547/making-sure-a-web-page-is-not-cached-across-all-browsers
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.setHeader('Pragma', 'no-cache')
+  res.setHeader('Expires', 0)
+  next()
+})
 .use(express.static(directory))
 .listen(port, () => console.log(`Serving ${directory} on port ${port}`))
